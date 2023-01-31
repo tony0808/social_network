683f10c82bf6dac9e8fe75030dc2da955aa1f3bd
Tony <csd4262@csd.uoc.gr>
2023-01-31 11:58:56 +0200

    Connecting to database

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
+// connect to db
+const dbURI = 'mongodb+srv://tony:tonycsd123@cluster0.7c8vm29.mongodb.net/social_network?retryWrites=true&w=majority';
+mongoose.set('strictQuery', true);
+mongoose.connect(dbURI)
+    .then(function(result) {
+        console.log('Connected to db');
+
+        // listen for incoming requests
+        app.listen(3000);
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+
 // register view engine
 app.set('view engine', 'ejs');
-
-// listen for incoming requests
-app.listen(3000);
 
 // routes
 app.get('/', function(req, res) {
